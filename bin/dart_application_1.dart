884c89f57cdd4cfc3b41b5de7481afaaba0e602d
vivektramachandran <vivektramachandran@gmail.com>
2023-09-21 00:44:26 +0530

    commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,19 +1,101 @@
-class Students{
-    //instace variable-globally declared variables
-    String? name;// ? -null aware - name may or may not be null
-    int? age;
-    int? phone;
-    String? email;
-    String? qualification;
-}
-void main(){
-    ///instance variable can be accessed outside the class only by using project
-    ///object creation -classname object_name = classname();  (classname() -constructor)
+// class Students{
+//     //instace variable-globally declared variables
+//     String? name;// ? -null aware - name may or may not be null
+//     int? age;
+//     int? phone;
+//     String? email;
+//     String? qualification;
+// }
+// void main(){
+//     ///instance variable can be accessed outside the class only by using project
+//     ///object creation -classname object_name = classname();  (classname() -constructor)
     
-    Students st1 = Students(); ///similar way => var st1 =students();
-    print("student 1 name =${st1.name ='Amal'}");
-    print('student 1 age =${st1.age =20}');
-    print('student 1 phone =${st1.phone = 1256342311}');
-    print('student 1 email =${st1.email = "amal@gmail.com"}');
-    print('student 1 qualification =${st1.qualification = 'BCA'}');
-}
+//     Students st1 = Students(); ///similar way => var st1 =students();
+//     print("student 1 name =${st1.name ='Amal'}");
+//     print('student 1 age =${st1.age =20}');
+//     print('student 1 phone =${st1.phone = 1256342311}');
+//     print('student 1 email =${st1.email = "amal@gmail.com"}');
+//     print('student 1 qualification =${st1.qualification = 'BCA'}');
+// }
+
+// class cars{
+//   String? name;
+//   int? price;
+//   dynamic colour;
+//   double? mileage;
+  
+//   }
+//   void main(){
+//     cars ct1=cars();
+//     print("ct1 1 name =${ct1.name ="etios"}");
+//     print("ct1 1 price =${ct1.price = 50000}");
+//     print("ct1 1 colour =${ct1.colour ="red"}");
+//     print("ct1 1 mileage =${ct1.mileage =15}");
+
+//     print("-------------------------------");
+//     cars ct2=cars();
+//     print("ct2 2 name =${ct2.name ="polo"}");
+//     print("ct2 2 price =${ct2.price =1000000}");
+//     print("ct2 2 colour =${ct2.colour ="white"}");
+//     print("ct2 2 mileage =${ct2.mileage =13}");
+    
+
+//   }
+
+ 
+// ///userdifined function
+//  void addition(){
+//   int a = 100, b = 200;///here a and b are local variables(locally declared variables)
+//   print("sum = ${a+b}");
+// }
+// void main(){
+//   addition();
+// }
+
+// void subtraction(){
+//   int a = 150, b = 75;
+//   print("sum = ${a-b}");
+// }
+// void main(){
+//   subtraction();
+// }
+
+// void multiplication(){
+//   int a = 10, b = 2;
+//   print("sum = ${a*b}");
+//   }
+//   void main(){
+//     multiplication();
+//   }
+
+// class flutter{
+//   int c =200;
+  
+//   void addition(){
+//     int a =100, b = 200;
+//     print ("sum =${a+b+c}");
+
+//   }
+//   }
+//  void main(){
+//   flutter obj = flutter();
+//   obj.addition();
+//  }
+//  import 'dart:io';
+
+// void main(){
+//   print("enter your name");
+//   String name = stdin.readLineSync()!;
+
+//   print("enter your age");
+//   int age = int.parse (stdin.readLineSync()!);
+  
+//   print("enter your cgpa");
+//   double cgpa = double.parse (stdin.readLineSync()!);
+  
+
+// print("name = $name");
+// print("age = $age");
+// print("cgpa) = $cgpa");
+// }
+ 
